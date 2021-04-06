d1b8c77e71599ebfa77ee11b59737425f353e38e
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-04-06 03:43:19 -1200

    post model, table aand controller

---
+++ database/migrations/2021_04_06_151602_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('course');
+            $table->string('task_title');
+            $table->string('description');
+            $table->string('task file');
+            $table->string('task points');
+            $table->string('time_status');
+            $table->string('submission status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
